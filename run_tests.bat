442f0afcc57fd3cd80f472e3394ec3ed740e7084
Varga Ákos <vrgakos@gmail.com>
2015-04-20 10:23:30 +0200

    Scripts

--- run_tests.bat
+++ run_tests.bat
@@ -1,6 +1,8 @@
 @echo off
+chcp 65001
 dir /s /B *.java > sources.txt
 mkdir classes
 javac -encoding UTF-8 -d classes @sources.txt
 del sources.txt
-jar cvfe szoftlab4.jar hu.bme.bitsplease.App -C classes .
+java -cp classes hu.bme.bitsplease.TestApp
+pause
